aaee1b20306ec97624e2edbe44ef16ffb6ed650d
gl1t4h <shyewilliams29@gmail.com>
2026-08-23 19:00:16 -0400

    Add login script with access key validation

---
+++ blank.js
@@ -0,0 +1,23 @@
+<script>
+const VALID_KEY = "xBac0pK-111";
+
+function showLogin() {
+  document.getElementById("key-login").style.display = "block";
+}
+
+function checkKey() {
+  const input = document.getElementById("accessKeyInput").value;
+  if (input === VALID_KEY) {
+    localStorage.setItem("loggedIn", "true");
+    location.reload();
+  } else {
+    document.getElementById("keyError").style.display = "block";
+  }
+}
+
+window.onload = () => {
+  if (localStorage.getItem("loggedIn") !== "true") {
+    showLogin();
+  }
+};
+</script>
